1b79c0b651a2272b20b9f5b4e8338eb29598d333
Anastasiia <anstasiakim2789@gmail.com>
2023-12-12 20:00:24 +0100

    added google font

--- src/Menu.js
+++ src/Menu.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import { Button, Divider, Flex, Typography } from 'antd';
 import { CalendarOutlined } from '@ant-design/icons';
-
+import "./App.css"
 let MenuComponent = () => {
   const [size, setSize] = useState('large')
   const { Title } = Typography;
 
   return (
     <Flex justify="space-between" align='center' style={{ height:"100%", width:"90%"}} >
-      <Flex>
-        <CalendarOutlined style={{fontSize: '35px'}}/>
-        <Title level={3}>MeetFlow Planner</Title>
+      <Flex  align='center' style={{textAlign:"center", height:"100%"}}>
+        <CalendarOutlined style={{fontSize: '35px', marginRight:10}}/>
+        <h3 >MeetFlow Planner</h3>
       </Flex>
       <Flex>
 
