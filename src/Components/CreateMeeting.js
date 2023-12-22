f581047d34c318b561c21cd230831ef4024398d3
Anastasiia <anstasiakim2789@gmail.com>
2023-12-22 12:38:08 +0100

    edited CreateMeeting component

--- src/Components/CreateMeeting.js
+++ src/Components/CreateMeeting.js
@@ -15,10 +15,9 @@ let CreateMeeting = () => {
         descriptions: '',
         location: '',
         time: '',
-        date: ''
+        date: '',
+        onlineConference: false
     });
-
-
 
     let CreateNewMeeting = async () => {
 
@@ -33,27 +32,11 @@ let CreateMeeting = () => {
 
         console.log(response)
     }
-    const MyFormItemContext = React.createContext([]);
-    function toArr(str) {
-        return Array.isArray(str) ? str : [str];
-    }
-
-    const MyFormItemGroup = ({ prefix, children }) => {
-        const prefixPath = React.useContext(MyFormItemContext);
-        const concatPath = React.useMemo(() => [...prefixPath, ...toArr(prefix)], [prefixPath, prefix]);
-        return <MyFormItemContext.Provider value={concatPath}>{children}</MyFormItemContext.Provider>;
-    };
-
-    const MyFormItem = ({ name, ...props }) => {
-        const prefixPath = React.useContext(MyFormItemContext);
-        const concatName = name !== undefined ? [...prefixPath, ...toArr(name)] : undefined;
-        return <Form.Item name={concatName} {...props} />;
-    };
 
     const onChange = (checked) => {
         setFormData({
             ...formData,
-            videoConference: checked
+            onlineConference: checked
         });
         console.log(checked)
     };
@@ -63,8 +46,19 @@ let CreateMeeting = () => {
             ...formData,
             date: value.format('YYYY-MM-DD')
         });
-       
+
     };
+    const onSelectCalendar = (date, { source }) => {
+        if (source === 'date') {
+            let dataNew = date.format('YYYY-MM-DD')
+            console.log('Panel Select:', source);
+            console.log(dataNew)
+            setFormData({
+                ...formData,
+                date: dataNew
+            });
+        }
+    }
     const wrapperStyle = {
         width: 300,
         border: `1px solid ${token.colorBorderSecondary}`,
@@ -88,128 +82,123 @@ let CreateMeeting = () => {
     };
     return (
         <Flex justify='center' style={{ width: "100%", minHeight: "100vh" }}>
-            <Flex vertical style={{ width: "70%", minHeight: "100vh", backgroundColor: "white", padding: 50 }}>
+            <Flex vertical justify='center' align='center' style={{ width: "70%", minHeight: "100vh", backgroundColor: "white", padding: 50 }}>
                 <Title style={{ width: "100%", height: "40px", borderBottom: "1px solid #D3DCE3", margin: 30 }} level={3}>Create group poll</Title>
-                <Form name="form_item_path" layout="vertical" onFinish={CreateNewMeeting}>
-                    <MyFormItemGroup prefix={['user']}>
-                        <MyFormItemGroup prefix={['name']}>
-                            <MyFormItem name="Title" label="Title">
-                                <Input onChange={(e) => { handleInputChange(e, "title") }} />
-                            </MyFormItem>
 
-                            <MyFormItem name="Description" label="Description (optional)">
-                                <Input onChange={(e) => { handleInputChange(e, "descriptions") }} />
-                            </MyFormItem>
-                        </MyFormItemGroup>
+                <Flex vertical style={{ width: "400px" }}>
 
-                        <MyFormItem name="Location" label="Location (optional)">
-                            <Input onChange={(e) => { handleInputChange(e, "location") }} />
-                        </MyFormItem>
+                    <Typography.Title level={5}>Title</Typography.Title>
+                    <Input value={formData.title} onChange={(e) => { handleInputChange(e, "title") }} />
 
-                        <MyFormItem name="VideoConferencing" label="Video conferencing">
-                            <Switch onChange={onChange} />
-                        </MyFormItem>
+                    <Typography.Title level={5}>Description</Typography.Title>
+                    <Input onChange={(e) => { handleInputChange(e, "descriptions") }} />
 
-                    </MyFormItemGroup>
+                    <Typography.Title level={5}>Location</Typography.Title>
+                    <Input onChange={(e) => { handleInputChange(e, "location") }} />
 
-                    <Title style={{ width: "100%", height: "40px", borderBottom: "1px solid #D3DCE3", margin: 30 }} level={3}>Add your times</Title>
+                    <Flex style={{ marginTop: 20 }}>
+                        <Typography.Title level={5} style={{ marginRight: 20 }}>Online conference</Typography.Title>
+                        <Switch style={{ width: "30px" }} onChange={onChange} />
+                    </Flex>
 
-                    <MyFormItemGroup prefix={['user']}>
-                        <MyFormItemGroup prefix={['name']}>
-                            <MyFormItem name="Duration" label="Duration">
-                                <TimePicker.RangePicker format="HH:mm" onChange={(e) => { handleInputChange(e, "time") }} />
-                            </MyFormItem>
-                            <div style={wrapperStyle}>
-                                <Calendar
-                                    fullscreen={false}
-                                    headerRender={({ value, type, onChange, onTypeChange }) => {
-                                        const start = 0;
-                                        const end = 12;
-                                        const monthOptions = [];
-                                        let current = value.clone();
-                                        const localeData = value.localeData();
-                                        const months = [];
-                                        for (let i = 0; i < 12; i++) {
-                                            current = current.month(i);
-                                            months.push(localeData.monthsShort(current));
-                                        }
-                                        for (let i = start; i < end; i++) {
-                                            monthOptions.push(
-                                                <Select.Option key={i} value={i} className="month-item">
-                                                    {months[i]}
-                                                </Select.Option>,
-                                            );
-                                        }
-                                        const year = value.year();
-                                        const month = value.month();
-                                        const options = [];
-                                        for (let i = year - 10; i < year + 10; i += 1) {
-                                            options.push(
-                                                <Select.Option key={i} value={i} className="year-item">
-                                                    {i}
-                                                </Select.Option>,
-                                            );
-                                        }
-                                        return (
-                                            <div
-                                                style={{
-                                                    padding: 8,
-                                                }}
-                                            >
-                                                <Typography.Title level={4}>Custom header</Typography.Title>
-                                                <Row gutter={8}>
-                                                    <Col>
-                                                        <Radio.Group
-                                                            size="small"
-                                                            onChange={(e) => onTypeChange(e.target.value)}
-                                                            value={type}
-                                                        >
-                                                            <Radio.Button value="month">Month</Radio.Button>
-                                                            <Radio.Button value="year">Year</Radio.Button>
-                                                        </Radio.Group>
-                                                    </Col>
-                                                    <Col>
-                                                        <Select
-                                                            size="small"
-                                                            dropdownMatchSelectWidth={false}
-                                                            className="my-year-select"
-                                                            value={year}
-                                                            onChange={(newYear) => {
-                                                                const now = value.clone().year(newYear);
-                                                                onChange(now);
-                                                            }}
-                                                        >
-                                                            {options}
-                                                        </Select>
-                                                    </Col>
-                                                    <Col>
-                                                        <Select
-                                                            size="small"
-                                                            dropdownMatchSelectWidth={false}
-                                                            value={month}
-                                                            onChange={(newMonth) => {
-                                                                const now = value.clone().month(newMonth);
-                                                                onChange(now);
-                                                            }}
-                                                        >
-                                                            {monthOptions}
-                                                        </Select>
-                                                    </Col>
-                                                </Row>
-                                            </div>
-                                        );
-                                    }}
-                                    onPanelChange={onPanelChange}
-                                />
-                            </div>
-                        </MyFormItemGroup>
-                        <Flex align='center' justify='center' style={{ width: "100%" }}>
-                            <Button type="primary" htmlType="submit" style={{ width: "50%", marginTop: "30px" }}>
-                                Submit
-                            </Button>
-                        </Flex>
-                    </MyFormItemGroup>
-                </Form>
+                </Flex>
+
+                <Title style={{ width: "100%", height: "40px", borderBottom: "1px solid #D3DCE3", margin: 30 }} level={3}>Add your times</Title>
+
+                <Flex vertical style={{ width: "400px" }}>
+                    <Typography.Title level={5}>Time of meeting</Typography.Title>
+                    <TimePicker.RangePicker style={{ marginBottom: 20 }} format="HH:mm" onChange={(e) => { handleInputChange(e, "time") }} />
+
+                    <Typography.Title level={5}>Date</Typography.Title>
+                    <div style={wrapperStyle}>
+                        <Calendar
+                            fullscreen={false}
+                            headerRender={({ value, type, onChange, onTypeChange }) => {
+                                const start = 0;
+                                const end = 12;
+                                const monthOptions = [];
+                                let current = value.clone();
+                                const localeData = value.localeData();
+                                const months = [];
+                                for (let i = 0; i < 12; i++) {
+                                    current = current.month(i);
+                                    months.push(localeData.monthsShort(current));
+                                }
+                                for (let i = start; i < end; i++) {
+                                    monthOptions.push(
+                                        <Select.Option key={i} value={i} className="month-item">
+                                            {months[i]}
+                                        </Select.Option>,
+                                    );
+                                }
+                                const year = value.year();
+                                const month = value.month();
+                                const options = [];
+                                for (let i = year - 10; i < year + 10; i += 1) {
+                                    options.push(
+                                        <Select.Option key={i} value={i} className="year-item">
+                                            {i}
+                                        </Select.Option>,
+                                    );
+                                }
+                                return (
+                                    <div
+                                        style={{
+                                            padding: 8,
+                                        }}
+                                    >
+
+                                        <Row gutter={8}>
+                                            <Col>
+                                                <Radio.Group
+                                                    size="small"
+                                                    onChange={(e) => onTypeChange(e.target.value)}
+                                                    value={type}
+                                                >
+                                                    <Radio.Button value="month">Month</Radio.Button>
+                                                    <Radio.Button value="year">Year</Radio.Button>
+                                                </Radio.Group>
+                                            </Col>
+                                            <Col>
+                                                <Select
+                                                    size="small"
+                                                    dropdownMatchSelectWidth={false}
+                                                    className="my-year-select"
+                                                    value={year}
+                                                    onChange={(newYear) => {
+                                                        const now = value.clone().year(newYear);
+                                                        onChange(now);
+                                                    }}
+                                                >
+                                                    {options}
+                                                </Select>
+                                            </Col>
+                                            <Col>
+                                                <Select
+                                                    size="small"
+                                                    dropdownMatchSelectWidth={false}
+                                                    value={month}
+                                                    onChange={(newMonth) => {
+                                                        const now = value.clone().month(newMonth);
+                                                        onChange(now);
+                                                    }}
+                                                >
+                                                    {monthOptions}
+                                                </Select>
+                                            </Col>
+                                        </Row>
+                                    </div>
+                                );
+                            }}
+                            onPanelChange={onPanelChange}
+                            onSelect={onSelectCalendar}
+                        />
+                    </div>
+                </Flex>
+
+                <Button onClick={CreateNewMeeting} type="primary" style={{ width: "100%", marginTop: "30px" }}>
+                    Submit
+                </Button>
             </Flex>
 
         </Flex>
